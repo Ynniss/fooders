6503da42ba8737c7ecf1f40121d67bb1d9a75209
ynniss <24393058+Ynniss@users.noreply.github.com>
2021-06-27 20:07:36 +0200

    fininshed the UI for the FAB's

--- app/src/main/java/com/esgi/fooders/HomeFragment.kt
+++ app/src/main/java/com/esgi/fooders/HomeFragment.kt
@@ -1,15 +1,45 @@
 package com.esgi.fooders
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import androidx.core.content.ContextCompat
 import com.esgi.fooders.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
+    private var fabAnalyzeClicked = false
+
+    private val rotateOpen: Animation by lazy {
+        AnimationUtils.loadAnimation(
+            requireContext(),
+            R.anim.rotate_open_animation
+        )
+    }
+    private val rotateClose: Animation by lazy {
+        AnimationUtils.loadAnimation(
+            requireContext(),
+            R.anim.rotate_close_animation
+        )
+    }
+    private val fromBottom: Animation by lazy {
+        AnimationUtils.loadAnimation(
+            requireContext(),
+            R.anim.from_bottom_animation
+        )
+    }
+    private val toBottom: Animation by lazy {
+        AnimationUtils.loadAnimation(
+            requireContext(),
+            R.anim.to_bottom_animation
+        )
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,7 +53,64 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.apply {
+            fabAnalyze.setOnClickListener {
+                onFabAnalyzeClicked()
+            }
+
+            fabManualEntry.setOnClickListener {
+                Log.d("fab", "manual entry")
+            }
+            fabScanBarcode.setOnClickListener {
+                Log.d("fab", "scan barcode")
+            }
+        }
     }
+
+    private fun onFabAnalyzeClicked() {
+        fabAnalyzeClicked = !fabAnalyzeClicked
+
+        binding.fabAnalyze.icon = if (fabAnalyzeClicked) {
+            ContextCompat.getDrawable(
+                requireContext(),
+                R.drawable.ic_button_close
+            )
+        } else {
+            ContextCompat.getDrawable(requireContext(), R.drawable.ic_button_analysis)
+        }
+
+        toggleVisibility()
+        setAnimations()
+    }
+
+    private fun toggleVisibility() {
+        if (fabAnalyzeClicked) {
+            binding.fabManualEntry.visibility = View.VISIBLE
+            binding.fabScanBarcode.visibility = View.VISIBLE
+        } else {
+            binding.fabManualEntry.visibility = View.GONE
+            binding.fabScanBarcode.visibility = View.GONE
+
+        }
+    }
+
+    private fun setAnimations() {
+        if (fabAnalyzeClicked) {
+            binding.apply {
+                fabManualEntry.startAnimation(fromBottom)
+                fabScanBarcode.startAnimation(fromBottom)
+                fabAnalyze.startAnimation(rotateOpen)
+            }
+        } else {
+            binding.apply {
+                fabManualEntry.startAnimation(toBottom)
+                fabScanBarcode.startAnimation(toBottom)
+                fabAnalyze.startAnimation(rotateClose)
+            }
+        }
+    }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
