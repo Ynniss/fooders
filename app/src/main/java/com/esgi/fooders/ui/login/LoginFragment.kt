6dea828578c9a7830d6203963dcb6ca7c9312b33
ynniss <24393058+Ynniss@users.noreply.github.com>
2021-07-17 20:10:48 +0200

    login UX now good

--- app/src/main/java/com/esgi/fooders/ui/login/LoginFragment.kt
+++ app/src/main/java/com/esgi/fooders/ui/login/LoginFragment.kt
@@ -1,18 +1,23 @@
 package com.esgi.fooders.ui.login
 
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
+import androidx.core.content.ContextCompat
+import androidx.core.content.ContextCompat.getSystemService
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import com.esgi.fooders.R
 import com.esgi.fooders.databinding.FragmentLoginBinding
-import dagger.hilt.EntryPoint
+import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
+
 
 @AndroidEntryPoint
 class LoginFragment : Fragment() {
@@ -33,19 +38,63 @@ class LoginFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.btnLogin.setOnClickListener {
-            loginViewModel.login("yannismekouche", "OhnonFOOD=77!")
+            onLoginButtonClick()
         }
 
         lifecycleScope.launchWhenStarted {
             loginViewModel.loginEvent.observe(viewLifecycleOwner, { event ->
                 when (event) {
-                    is LoginViewModel.LoginEvent.Success -> findNavController().navigate(R.id.action_loginFragment_to_homeFragment)
-                    is LoginViewModel.LoginEvent.Failure -> Log.d("FAILURE", event.error)
-                    is LoginViewModel.LoginEvent.Loading -> Log.d("LOADING", "LOADING")
-                    else -> Log.d("ELSE", event.toString())
+                    is LoginViewModel.LoginEvent.Success -> {
+                        binding.apply {
+                            lottieFoodLoading.visibility = View.GONE
+                            lottieFoodLoading.cancelAnimation()
+                        }
+                        findNavController().navigate(R.id.action_loginFragment_to_homeFragment)
+                    }
+                    is LoginViewModel.LoginEvent.Failure -> {
+                        binding.apply {
+                            lottieFoodLoading.visibility = View.GONE
+                            lottieFoodLoading.cancelAnimation()
+                        }
+                        Snackbar.make(binding.root, event.error, Snackbar.LENGTH_SHORT)
+                            .show()
+                    }
+                    is LoginViewModel.LoginEvent.Loading -> {
+                        Log.d("LOADING", "LOADING")
+                        binding.apply {
+                            lottieFoodLoading.visibility = View.VISIBLE
+                            lottieFoodLoading.playAnimation()
+                        }
+                    }
+                    else -> Unit
                 }
             })
         }
+    }
+
+    private fun onLoginButtonClick() {
+        closeKeyboard()
+        binding.inputEmailLayout.error = null
+        binding.inputPasswordLayout.error = null
+
+        if (!binding.inputEmail.text.isNullOrEmpty() && !binding.inputPassword.text.isNullOrEmpty()) {
+            loginViewModel.login(
+                binding.inputEmail.text.toString(),
+                binding.inputPassword.text.toString()
+            )
+        }
+        if (binding.inputEmail.text.isNullOrEmpty()) {
+            binding.inputEmailLayout.error = "Email Cannot be empty !"
+        }
+        if (binding.inputPassword.text.isNullOrEmpty()) {
+            binding.inputPasswordLayout.error = "Password Cannot be empty !"
+        }
+    }
+
+    private fun closeKeyboard() {
+        val imm = getSystemService(binding.root.context, InputMethodManager::class.java)
+        imm?.hideSoftInputFromWindow(binding.root.windowToken, 0)
+
     }
 
     override fun onDestroyView() {
