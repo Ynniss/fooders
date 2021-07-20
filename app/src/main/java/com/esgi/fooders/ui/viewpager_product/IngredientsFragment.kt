9a9399d1ff099e58e722b4aae8e46986a7292643
Yannis <yaanis213@gmail.com>
2021-07-20 21:54:47 +0200

    WIP viewpager integration, to display product informations on bottom Sheet !

---
+++ app/src/main/java/com/esgi/fooders/ui/viewpager_product/IngredientsFragment.kt
@@ -0,0 +1,20 @@
+package com.esgi.fooders.ui.viewpager_product
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.esgi.fooders.R
+
+class IngredientsFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_ingredients, container, false)
+    }
+
+}
