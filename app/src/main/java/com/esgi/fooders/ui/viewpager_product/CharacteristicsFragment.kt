13dbe8dafbdccc506e3bf54f0b55d55284cda43d
ynniss <24393058+Ynniss@users.noreply.github.com>
2021-07-22 16:39:45 +0200

    WIP fix camera workflow

--- app/src/main/java/com/esgi/fooders/ui/viewpager_product/CharacteristicsFragment.kt
+++ app/src/main/java/com/esgi/fooders/ui/viewpager_product/CharacteristicsFragment.kt
@@ -1,12 +1,9 @@
 package com.esgi.fooders.ui.viewpager_product
 
 import android.os.Bundle
-import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.GridLayout
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.bumptech.glide.Glide
@@ -53,26 +50,8 @@ class CharacteristicsFragment : Fragment() {
         binding.apply {
             Glide.with(requireContext()).load(data.data.image_nutrition_url)
                 .into(imgNutrimentsList)
+
+
         }
-        val titleText: TextView
-        val gridLayout = GridLayout(context)
-        gridLayout.setAlignmentMode(GridLayout.ALIGN_BOUNDS)
-        gridLayout.setColumnCount(2)
-        gridLayout.setRowCount(3)
-
-        titleText = TextView(context)
-        titleText.setText("toto")
-        gridLayout.addView(titleText, 0)
-        val param: GridLayout.LayoutParams = GridLayout.LayoutParams()
-        param.height = GridLayout.LayoutParams.WRAP_CONTENT
-        param.width = GridLayout.LayoutParams.WRAP_CONTENT
-        param.rightMargin = 5
-        param.topMargin = 5
-        param.setGravity(Gravity.CENTER)
-        param.columnSpec = GridLayout.spec(0)
-        param.rowSpec = GridLayout.spec(0)
-        titleText.layoutParams = param
-
-        binding.root.addView(gridLayout)
     }
 }
