dc3b06fffe8924230954e9db581e50f61a0784d4
Yannis <yaanis213@gmail.com>
2021-07-24 23:29:17 +0200

    added missing File class

---
+++ app/src/main/java/com/esgi/fooders/data/remote/responses/ModifyProductImageResponse/File.kt
@@ -0,0 +1,9 @@
+package com.esgi.fooders.data.remote.responses.ModifyProductImageResponse
+
+data class File(
+    val code: String,
+    val filename: String,
+    val name: String,
+    val thumbnailUrl: String,
+    val url: String
+)
