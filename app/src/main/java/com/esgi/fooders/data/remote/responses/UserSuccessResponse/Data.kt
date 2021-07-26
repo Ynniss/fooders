066825f16e4f7836563d995372e69e8f4eee5437
Yannis <yaanis213@gmail.com>
2021-07-26 23:09:52 +0200

    wip

---
+++ app/src/main/java/com/esgi/fooders/data/remote/responses/UserSuccessResponse/Data.kt
@@ -0,0 +1,6 @@
+package com.esgi.fooders.data.remote.responses.UserSuccessResponse
+
+data class Data(
+    val last_updated_at: Long,
+    val unlocked: Unlocked
+)
--- app/src/main/java/com/esgi/fooders/data/remote/responses/UserSuccessResponse/UserSuccessResponse.kt
+++ app/src/main/java/com/esgi/fooders/data/remote/responses/UserSuccessResponse/UserSuccessResponse.kt
@@ -1,10 +1,5 @@
 package com.esgi.fooders.data.remote.responses.UserSuccessResponse
 
-
 data class UserSuccessResponse(
     val data: Data
-)
-
-data class Data(
-    val unlocked: List<String>
 )
