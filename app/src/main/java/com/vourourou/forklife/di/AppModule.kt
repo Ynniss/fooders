839d81b90abbafc26da43e31937ae0187e10c19d
ynniss <24393058+Ynniss@users.noreply.github.com>
2026-01-07 22:54:59 +0100

    Add in-app reviews + Allergen alerts

--- app/src/main/java/com/vourourou/forklife/di/AppModule.kt
+++ app/src/main/java/com/vourourou/forklife/di/AppModule.kt
@@ -2,6 +2,7 @@ package com.vourourou.forklife.di
 
 import android.content.Context
 import androidx.room.Room
+import com.google.gson.Gson
 import com.vourourou.forklife.BuildConfig
 import com.vourourou.forklife.data.local.ForkLifeDatabase
 import com.vourourou.forklife.data.local.ScanHistoryDao
@@ -64,7 +65,10 @@ object AppModule {
             ForkLifeDatabase::class.java,
             "forklife_database"
         )
-            .addMigrations(ForkLifeDatabase.MIGRATION_1_2)
+            .addMigrations(
+                ForkLifeDatabase.MIGRATION_1_2,
+                ForkLifeDatabase.MIGRATION_2_3
+            )
             .build()
     }
 
@@ -76,8 +80,17 @@ object AppModule {
 
     @Singleton
     @Provides
-    fun provideHistoryRepository(scanHistoryDao: ScanHistoryDao): HistoryRepository {
-        return HistoryRepository(scanHistoryDao)
+    fun provideGson(): Gson {
+        return Gson()
+    }
+
+    @Singleton
+    @Provides
+    fun provideHistoryRepository(
+        scanHistoryDao: ScanHistoryDao,
+        gson: Gson
+    ): HistoryRepository {
+        return HistoryRepository(scanHistoryDao, gson)
     }
 
 }
